a1001e721492237b1e15abe6ac67be5c0423183d
StefanMilanovic <33437717+StefanMilanovic@users.noreply.github.com>
2018-02-10 19:17:26 +0100

    Update vektori.py

--- StefanProjekat/vektori.py
+++ StefanProjekat/vektori.py
@@ -45,24 +45,6 @@ def add(v, w):
     return (x + X, y + Y)
 
 
-# Given a line with coordinates 'start' and 'end' and the
-# coordinates of a point 'pnt' the proc returns the shortest
-# distance from pnt to the line and the coordinates of the
-# nearest point on the line.
-#
-# 1  Convert the line segment to a vector ('line_vec').
-# 2  Create a vector connecting start to pnt ('pnt_vec').
-# 3  Find the length of the line vector ('line_len').
-# 4  Convert line_vec to a unit vector ('line_unitvec').
-# 5  Scale pnt_vec by line_len ('pnt_vec_scaled').
-# 6  Get the dot product of line_unitvec and pnt_vec_scaled ('t').
-# 7  Ensure t is in the range 0 to 1.
-# 8  Use t to get the nearest location on the line to the end
-#    of vector pnt_vec_scaled ('nearest').
-# 9  Calculate the distance from nearest to pnt_vec_scaled.
-# 10 Translate nearest back to the start/end line.
-# Malcolm Kesson 16 Dec 2012
-
 def pnt2line(pnt, start, end):
     
     line_vec = vector(start, end)
@@ -76,7 +58,7 @@ def pnt2line(pnt, start, end):
     pnt_vec_scaled = scale(pnt_vec, 1.0 / line_len)
 
     t = dot(line_unitvec, pnt_vec_scaled)
-    #print "t: ", t
+
     r = 1
     if t < 0.0:
         t = 0.0
@@ -90,5 +72,5 @@ def pnt2line(pnt, start, end):
     nearest = scale(line_vec, t)
     dist = distance(nearest, pnt_vec)
     nearest = add(nearest, start)
-    return dist
+    return (dist, (int(nearest[0]), int(nearest[1])), r)
 
